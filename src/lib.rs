80ce247069ec72d11be6912b70365c8eab00bb1d
Jordan Isaacs <mail@jdisaacs.com>
2023-03-01 18:47:26 -0500

    update & test readme

--- src/lib.rs
+++ src/lib.rs
@@ -656,3 +656,17 @@ impl<S: squeue::EntryMarker, C: cqueue::EntryMarker> AsFd for IoUring<S, C> {
         self.fd.as_fd()
     }
 }
+
+// Test the readme code
+// From: https://github.com/rust-lang/cargo/issues/383#issuecomment-720873790
+#[cfg(doctest)]
+mod test_readme {
+    macro_rules! external_doc_test {
+        ($x:expr) => {
+            #[doc = $x]
+            extern "C" {}
+        };
+    }
+
+    external_doc_test!(include_str!("../README.md"));
+}
