fb522d25d05a8b39dc9f87a640afe0d45c09c307
Jordan Isaacs <mail@jdisaacs.com>
2023-03-01 18:47:26 -0500

    update & test readme

--- src/lib.rs
+++ src/lib.rs
@@ -631,3 +631,17 @@ impl AsFd for IoUring {
         self.fd.as_fd()
     }
 }
+
+// Test the readme code
+// From: https://github.com/rust-lang/cargo/issues/383#issuecomment-720873790
+#[cfg(doctest)]
+mod test_readme {
+    macro_rules! external_doc_test {
+        ($x:expr) => {
+            #[doc = $x]
+            extern "C" {}
+        };
+    }
+
+    external_doc_test!(include_str!("../README.md"));
+}
