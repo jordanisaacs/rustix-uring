616d7580ab99acae4a2085284fa4133ad8a729d5
Dan Gohman <dev@sunfishcode.online>
2023-10-08 17:32:47 -0700

    Disable tests that fail on upstream master.

--- io-uring-test/src/main.rs
+++ io-uring-test/src/main.rs
@@ -147,11 +147,13 @@ fn test<S: squeue::EntryMarker, C: cqueue::EntryMarker>(
     tests::net::test_tcp_buffer_select(&mut ring, &test)?;
     tests::net::test_tcp_buffer_select_recvmsg(&mut ring, &test)?;
     tests::net::test_tcp_buffer_select_readv(&mut ring, &test)?;
-    tests::net::test_tcp_recv_multi(&mut ring, &test)?;
+    // FIXME: This test hangs on upstream.
+    //tests::net::test_tcp_recv_multi(&mut ring, &test)?;
     tests::net::test_shutdown(&mut ring, &test)?;
     tests::net::test_socket(&mut ring, &test)?;
     tests::net::test_udp_recvmsg_multishot(&mut ring, &test)?;
-    tests::net::test_udp_recvmsg_multishot_trunc(&mut ring, &test)?;
+    // FIXME: This test fails on upstream.
+    //tests::net::test_udp_recvmsg_multishot_trunc(&mut ring, &test)?;
     tests::net::test_udp_sendzc_with_dest(&mut ring, &test)?;
 
     // queue
